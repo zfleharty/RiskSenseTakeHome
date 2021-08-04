aca09aa428e75ba78d2882053b21a2f635c62bdd
zfleharty <zfleharty@unm.edu>
2021-08-03 20:50:26 -0600

    Added hashmap to Animal.Java so each animal may have an arbitrary number of quailities added to them and added functions for setting and getting a quality as well as method for pretty printing the keys and values in the HashMap.

--- problem2/src/Animal.java
+++ problem2/src/Animal.java
@@ -1,9 +1,31 @@
+import java.util.HashMap;
+import java.util.Set;
+
 public abstract class Animal {
     enum Size{SMALL,MEDIUM,LARGE}
     private String name;
-    private Size size;
 
+    private HashMap<String,String> Attributes;
+
+    public void setAttribute(String key, String value){
+        Attributes.put(key,value);
+    }
+
+    public String getAttribute(String key){
+        return Attributes.get(key);
+            //Should add in condition if key not present (perhaps exception or default return value)
+    }
     
+    //Perhaps useful function to pretty print an animal and all its set attributes
+    public void getAttributes(){
+        Set<String> keys = Attributes.keySet();
+        System.out.println(name + "{");
+        for(String k:keys){
+            System.out.println("\t" + k + ": " + Attributes.get(k));
+        }
+
+        System.out.println("}");
+    }
     public Size getSize() {
         return size;
     }
@@ -12,6 +34,7 @@ public abstract class Animal {
         this.size = size;
     }
 
+    private Size size;
     public String getName()
         {
             return name;
@@ -21,4 +44,6 @@ public abstract class Animal {
         name = nameGiven;
     }
 
+
 }
+
